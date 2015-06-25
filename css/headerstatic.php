bbb5b496b427baa46e74d2f241eb846dc042c36d
greatscotts <javelonagian@live.com>
2015-06-25 10:21:28 +0800

    Eqwe

---
+++ css/headerstatic.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="">
+<meta name="keywords" content=""/>
+<meta name="author" content="Jason Christopher Castro, Kitti2x Designs">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>OrangeApps Inc.-Education technology</title>
+
+<link href="../bootstrap-3.1.1-dist/css/bootstrap.css" rel="stylesheet" media="screen">
+<link href="../css/topNav.css" rel="stylesheet" media="screen">
+<link href="../css/khawna-styles.css" rel="stylesheet" media="screen">
+</head>
+<body>
+
+<script type="text/javascript">
+   if (screen.width <= 860) {
+    window.location = "http://orangeapps.ph/mobilehome.html";
+  }
+</script>
+
+
+<main class="cd-main-content">
+    <nav class="  navbar-inverse" role="navigation">
+      <a class="navbar-brand" href="../index.php"><img src="../images/headerlogowhite.png" style="margin-left:99px; margin-top:19px;"></a>
+        <ul class="nav navbar-nav navbar-right">
+          <nav id="cd-top-nav" class="hidden-xs">
+        <ul class="nav">
+     <li><a href="why.html" style="font-size:18px;">How it Works</a></li>
+             <li><a href="features.html" style="font-size:18px;">Features</a></li>
+                <li><a href="pricing.html"style="font-size:18px;">Pricing</a></li>
+           <li><a href="about.html" style="font-size:18px;">About</a></li>
+     
+          <li><a href="contactus.php"style="font-size:18px;">Contact Us</a></li>
+<li><a type="button" class="btn btn-warning btn-lg btn-block"  style="font-weight:bold; width:80px;  margin-top:-10px;"href="login.html">Login</a></li>
+ 
+        </ul> 
+      </nav>
+      </nav>   
